91f591746f8d1c1e1cf31f259c0648ac723066ef
H.M.E <1316126217@qq.com>
2021-07-17 22:41:30 +0800

    创建分类完成

--- admin/src/main.js
+++ admin/src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import './plugins/element.js'
 
 Vue.config.productionTip = false
+import http from './http'
+Vue.prototype.$http = http
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
---
+++ server/index.js
@@ -0,0 +1,16 @@
+const  express= require("express")
+
+const  app=express()
+
+app.use(require('cors')())
+// 中间键
+app.use(express.json())
+
+
+
+require('./router/admin')(app)
+require('./plugins/db')(app)
+
+app.listen(3000,()=>{
+  console.log('http://localhost:3000')
+})
