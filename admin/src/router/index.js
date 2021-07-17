c5510e302a672f123c1ed2c803c9694359d891c5
H.M.E <1316126217@qq.com>
2021-07-17 23:12:57 +0800

    获取分类显示

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Main from '../views/Main.vue'
 import CreategoryEdit from '../views/CreategoryEdit.vue'
+import CategoryList from '../views/CategoryList.vue'
 
 Vue.use(VueRouter)
 
@@ -11,7 +12,8 @@ const routes = [
     name: 'main',
     component: Main,
     children: [
-      {path:"/categories/create",component:CreategoryEdit}
+      { path: "/categories/create", component: CreategoryEdit },
+      { path: "/categories/list", component: CategoryList }
     ]
   },
 
--- server/router/admin/index.js
+++ server/router/admin/index.js
@@ -4,10 +4,17 @@ module.exports = app => {
   const router = express.Router()
 
   const Category = require('../../models/Category')
+
+  // 存入数据库
   router.post('/categories', async (req, res) => {
     const model = await Category.create(req.body)
     res.send(model)
+  })
 
+  // 得到数据
+  router.get('/categories', async (req, res) => {
+    const items = await Category.find().limit(10)
+    res.send(items)
   })
   app.use('/admin/api', router)
 }
