db26f6465d033e20add5f3320e65b27afe57e72d
H.M.E <1316126217@qq.com>
2021-12-28 21:28:39 +0800

    add  首页轮播图和解决swiper版本引入圆点没有的问题

--- web/src/main.js
+++ web/src/main.js
@@ -4,16 +4,15 @@ import App from './App.vue'
 import VueAwesomeSwiper from 'vue-awesome-swiper'
 
 // import style (>= Swiper 6.x)
-import 'swiper/swiper-bundle.css'
-
+// import 'swiper/swiper-bundle.css'
+import 'swiper/dist/css/swiper.css'
 // import style (<= Swiper 5.x)
 // import 'swiper/css/swiper.css'
 
 Vue.use(VueAwesomeSwiper)
+
 Vue.config.productionTip = false
-
-
-import './style.scss'
+import './assets/scss/style.scss'
 import router from './router'
 new Vue({
   router,
--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,22 +1,54 @@
 <template>
   <div>
-<swiper >
-    <swiper-slide>Slide 1</swiper-slide>
-    <swiper-slide>Slide 2</swiper-slide>
-    <swiper-slide>Slide 3</swiper-slide>
-    <swiper-slide>Slide 4</swiper-slide>
-    <swiper-slide>Slide 5</swiper-slide>
-
-  </swiper>
+    <swiper :options="swiperOptions">
+      <swiper-slide>
+        <img class="w-100" src="../assets/images/banner1.jpeg" alt="" />
+      </swiper-slide>
+      <swiper-slide>
+        <img class="w-100" src="../assets/images/banner2.jpeg" alt="" />
+      </swiper-slide>
+      <swiper-slide>
+        <img class="w-100" src="../assets/images/banner3.jpeg" alt="" />
+      </swiper-slide>
+      <div
+        class="swiper-pagination pagination-home text-right
+        px-3  pb-1"
+        slot="pagination"
+      ></div>
+    </swiper>
   </div>
 </template>
 
 <script>
+// import Swiper, { Pagination, Navigation } from 'swiper'
+// Swiper.use([Pagination, Navigation])
 export default {
+  data() {
+    return {
+      swiperOptions: {
+        loop: true,
+        pagination: {
+          el: '.pagination-home',
 
+          clickable: true,
+        },
+        // Some Swiper option/callback...
+      }
+    }
+  }
 }
 </script>
 
-<style>
-
+<style lang="scss">
+@import "../assets/scss/variables";
+.pagination-home {
+  .swiper-pagination-bullet {
+    opacity: 1;
+    border-radius: .125rem;
+    background: map-get($map: $colors, $key: "white");
+    &.swiper-pagination-bullet-active {
+      background: map-get($colors, "info");
+    }
+  }
+}
 </style>
